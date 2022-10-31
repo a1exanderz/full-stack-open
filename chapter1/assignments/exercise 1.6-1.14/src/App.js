a1b3514fa28de85104de222032a284b73026ff71
Alexander Zhang <awz2106@columbia.edu>
2022-10-31 13:05:43 -0700

    Complete exercise 1.8

--- chapter1/assignments/exercise 1.6-1.14/src/App.js
+++ chapter1/assignments/exercise 1.6-1.14/src/App.js
@@ -4,6 +4,20 @@ const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <div> good {good}</div>
+      <div> neutral {neutral}</div>
+      <div> bad {bad}</div>
+      <div> all {good + neutral + bad}</div>
+      <div> average {(good - bad) / (good + neutral + bad)}</div>
+      <div> positive {(100 * good) / (good + neutral + bad)} %</div>
+    </div>
+  );
+};
+
 function App() {
   const [good, incrementGood] = useState(0);
   const [neutral, incrementNeutral] = useState(0);
@@ -16,14 +30,8 @@ function App() {
         <Button onClick={() => incrementGood(good + 1)} text="good" />
         <Button onClick={() => incrementNeutral(neutral + 1)} text="neutral" />
         <Button onClick={() => incrementBad(bad + 1)} text="bad" />
+        <Statistics good={good} neutral={neutral} bad={bad} />
       </div>
-      <h1>statistics</h1>
-      <div> good {good}</div>
-      <div> neutral {neutral}</div>
-      <div> bad {bad}</div>
-      <div> all {good + neutral + bad}</div>
-      <div> average {(good - bad) / (good + neutral + bad)}</div>
-      <div> positive {(100 * good) / (good + neutral + bad)} %</div>
     </div>
   );
 }
