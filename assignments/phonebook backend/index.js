3c217643d038f00e68e2dd9ff76b75650a44a3df
Alexander Zhang <awz2106@columbia.edu>
2022-11-05 17:13:08 -0700

    Complete exercise 3.2

--- assignments/phonebook backend/index.js
+++ assignments/phonebook backend/index.js
@@ -32,6 +32,13 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  response.send(
+    `<div>Phonebook has info for ${persons.length} people</div>
+    <p>${Date()}</p>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server is running on ${PORT}`);
