d41360bdcf08b7d1c855547a5ae0508ab2407152
Alexander Zhang <awz2106@columbia.edu>
2022-11-16 23:03:58 -0800

    Work on blog list backend

--- assignments/blog list/blog list frontend/src/services/blogEntryService.js
+++ assignments/blog list/blog list frontend/src/services/blogEntryService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3006/blog-entries";
+const baseUrl = "/api/blog-entries";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
