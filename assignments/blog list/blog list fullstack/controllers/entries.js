3748a40b9be02a5062661c43e1c03a7a2889e3c3
Alexander Zhang <awz2106@columbia.edu>
2022-11-17 15:58:19 -0800

    Complete bloglist fullstack exercises 4.1-4.2

---
+++ assignments/blog list/blog list fullstack/controllers/entries.js
@@ -0,0 +1,65 @@
+const entriesRouter = require("express").Router();
+const Entry = require("../models/entry");
+
+entriesRouter.get("/", (request, response) => {
+  Entry.find({}).then((entries) => {
+    response.json(entries);
+  });
+});
+
+entriesRouter.get("/:id", (request, response, next) => {
+  Entry.findById(request.params.id)
+    .then((entry) => {
+      if (entry) {
+        response.json(entry);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+entriesRouter.post("/", (request, response, next) => {
+  const body = request.body;
+
+  const entry = new Entry({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
+
+  entry
+    .save()
+    .then((savedEntry) => {
+      response.json(savedEntry);
+    })
+    .catch((error) => next(error));
+});
+
+entriesRouter.delete("/:id", (request, response, next) => {
+  Entry.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+entriesRouter.put("/:id", (request, response, next) => {
+  const body = request.body;
+
+  const entry = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  Entry.findByIdAndUpdate(request.params.id, entry, { new: true })
+    .then((updatedEntry) => {
+      response.json(updatedEntry);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = entriesRouter;
---
+++ assignments/blog list/blog list fullstack/models/entry.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const entrySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: false,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+});
+
+entrySchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Entry", entrySchema);
