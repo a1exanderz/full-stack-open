3143f1920aca861a9b6ba2a8827ea322fed8e81b
Alexander Zhang <awz2106@columbia.edu>
2022-11-17 22:27:49 -0800

    Complete exercise 4.6

--- assignments/blog list/blog list fullstack/utils/list_helper.js
+++ assignments/blog list/blog list fullstack/utils/list_helper.js
@@ -14,8 +14,8 @@ const favoriteBlog = (entries) => {
 
   let favoriteBlog = entries.filter((entry) => entry.likes === mostLikes);
 
-  const transformObject = (array) => {
-    const object = array[0];
+  const transformObject = (entries) => {
+    const object = entries[0];
     delete object._id;
     delete object.__v;
     delete object.url;
@@ -26,8 +26,29 @@ const favoriteBlog = (entries) => {
   return transformObject(favoriteBlog);
 };
 
+const mostBlogs = (entries) => {
+  const map = new Map();
+
+  let mostBlogs = [0, ""];
+  for (let i = 0; i < entries.length; i++) {
+    map.set(entries[i].author, (map.get(entries[i].author) ?? 0) + 1);
+
+    if (map.get(entries[i].author) > mostBlogs[0]) {
+      mostBlogs = [map.get(entries[i].author), entries[i].author];
+    }
+  }
+
+  const testOutput = {
+    author: mostBlogs[1],
+    blogs: mostBlogs[0],
+  };
+
+  return testOutput;
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
