929b41b712195e5f778435b0a2c5010ab5496ac9
Alexander Zhang <awz2106@columbia.edu>
2022-11-17 21:11:59 -0800

    Complete exercise 4.4

--- assignments/blog list/blog list fullstack/utils/list_helper.js
+++ assignments/blog list/blog list fullstack/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (entries) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, entry) => sum + entry.likes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
