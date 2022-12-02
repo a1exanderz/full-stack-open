58ec0ed2fd24d70d513b216a7e40f75ffeaaf493
Alexander Zhang <awz2106@columbia.edu>
2022-12-02 02:01:02 -0800

    Complete tutorial up to test file location

--- demos/frontend notes/src/App.js
+++ demos/frontend notes/src/App.js
@@ -64,7 +64,7 @@ const App = () => {
       .then((returnedNote) => {
         setNotes(notes.map((n) => (n.id !== id ? n : returnedNote)));
       })
-      .catch((error) => {
+      .catch(() => {
         setErrorMessage(
           `Note '${note.content}' was already removed from server`
         );
