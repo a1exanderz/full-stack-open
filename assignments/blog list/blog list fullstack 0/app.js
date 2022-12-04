b35cf2e84c2547f81571d5c8f1204a61a01caaab
Alexander Zhang <awz2106@columbia.edu>
2022-12-04 11:35:37 -0800

    Update for 5d exercises

--- assignments/blog list/blog list fullstack 0/app.js
+++ assignments/blog list/blog list fullstack 0/app.js
@@ -33,7 +33,7 @@ app.use("/api/users", usersRouter);
 
 if (process.env.NODE_ENV === "test") {
   const testingRouter = require("./controllers/testing");
-  app.use("api/testing", testingRouter);
+  app.use("/api/testing", testingRouter);
 }
 
 app.use(middleware.unknownEndpoint);
