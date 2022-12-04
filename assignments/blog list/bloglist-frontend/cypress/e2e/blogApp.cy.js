6b1e22264a3171e42a16c4c7e745abf6a70b9362
Alexander Zhang <awz2106@columbia.edu>
2022-12-04 10:17:49 -0800

    Complete exercise 5.18

--- assignments/blog list/bloglist-frontend/cypress/e2e/blogApp.cy.js
+++ assignments/blog list/bloglist-frontend/cypress/e2e/blogApp.cy.js
@@ -1,22 +1,29 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3001/api/testing/reset");
+    const user = {
+      username: "alexz648",
+      password: "alexz648",
+    };
+    cy.request("POST", "http://localhost:3001/api/users", user);
     cy.visit("http://localhost:3000");
   });
   it("login form is shown", function () {
     cy.contains("log in");
   });
+
+  describe("Login", function () {
+    it("succeeds with right creds", function () {
+      cy.get("#username").type("alexz648");
+      cy.get("#password").type("alexz648");
+      cy.get("#login-button").click();
+      cy.contains("blogs");
+    });
+    it("fails with wrong creds", function () {
+      cy.get("#username").type("alexz648");
+      cy.get("#password").type("alexz647");
+      cy.get("#login-button").click();
+      cy.contains("Wrong credentials");
+    });
+  });
 });
-
-// describe("Blog app 2", function () {
-//   beforeEach(function () {
-//     cy.visit("http://localhost:3000");
-//     cy.get("#username").type("alexz648");
-//     cy.get("#password").type("alexz648");
-//     cy.get("#login-button").click();
-//   });
-
-//   it("open front page", function () {
-//     cy.contains("blogs");
-//   });
-// });
