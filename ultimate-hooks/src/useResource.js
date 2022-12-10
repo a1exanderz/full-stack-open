192b89b9d4edd6fa5ff460c79683b1bfc71240b8
Alexander Zhang <awz2106@columbia.edu>
2022-12-10 10:07:53 -0800

    Complete exercise 7.8

--- ultimate-hooks/src/useResource.js
+++ ultimate-hooks/src/useResource.js
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([]);
 
-  // ...
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(baseUrl);
+      setResources(response.data);
+    };
+
+    fetchData();
+  }, []);
 
   const create = (resource) => {
-    // ...
+    const newResource = { ...resource, id: resources.length + 1 };
+    setResources([...resources, newResource]);
   };
 
   const service = {
